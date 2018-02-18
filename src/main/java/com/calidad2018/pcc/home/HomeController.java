78e68686dc724406bcc32397d869ad4074d8f783
esteban <estebantcs@gmail.com>
2018-02-17 22:48:51 -0500

    added check employees size and retur to home or form

--- src/main/java/com/calidad2018/pcc/home/HomeController.java
+++ src/main/java/com/calidad2018/pcc/home/HomeController.java
@@ -1,19 +1,37 @@
 package com.calidad2018.pcc.home;
 
 import com.calidad2018.pcc.employee.Employee;
+import com.calidad2018.pcc.employee.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployeeServiceImpl employeeService;
 
     @RequestMapping("/")
        public String home(Model model) {
 
+        List<Employee> employeeList = (List<Employee>) employeeService.findAll();
+
+        if(employeeList.size() == 0){
+
+            return "home/noEmployee";
 
 
-        return "home/noEmployee";
+        }
+        else {
+            return "home/index";
+
+
+        }
+
     }
 
 
