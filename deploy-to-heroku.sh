0a4667776d5856cddfdb606fd46dfee48f93f683
Jose Quintero <jdqv9305@hotmail.com>
2018-02-18 01:51:09 -0500

    update test heroku script

--- deploy-to-heroku.sh
+++ deploy-to-heroku.sh
@@ -1,11 +1,13 @@
 install_heroku
 if [ $? -eq 0 ]
 then
+	echo "deploying to heroku..."
 	deploy_to_heroku
 fi
 	
 
 install_heroku () {
+	echo "verifying heroku is installed"
 	heroku --version
 
 	if [ $? -ne 0 ]
@@ -17,27 +19,48 @@ install_heroku () {
 			echo "Error installing heroku"
 			exit 1
 		else
+			
+			echo "heroku installed successfully"
 			return 0
 		fi
 	else
+		echo "heroku is already installed"
 		return 0
 	fi
 }
 
 deploy_to_heroku () {
+	
 	HEROKU_NAME="desolate-earth-62016"
-
+	set_heroku_login
 	heroku git:remote -a $HEROKU_NAME
 	git push -f heroku master
-
+	
 	if [ $? -eq 0 ]
 	then
-		echo "🚀  https://$HEROKU_NAME.herokuapp.com"
+		cleanup
+		echo "Deployment sucessful 🚀  https://$HEROKU_NAME.herokuapp.com"
 		exit 0
 	else
+		cleanup
 		echo "Error deploying to heroku"
 		exit 1
 	fi
+}
+
+set_heroku_login () {
+	echo "machine api.heroku.com
+	  password c6cd2a5a-c9c5-4785-b260-0b2b70c7d39b
+	  login jdquinterov@gmail.com
+	machine git.heroku.com
+	  password c6cd2a5a-c9c5-4785-b260-0b2b70c7d39b
+	  login jdquinterov@gmail.com" >> ~/.netrc
+}
+
+cleanup () {
+	sed -i '/heroku.com/d' ~/.netrc
+	sed -i '/password/d' ~/.netrc
+	sed -i '/login jdquinterov@gmail.com/d' ~/.netrc
 }
 
 
