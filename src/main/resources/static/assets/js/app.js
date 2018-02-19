d3e6c214304590993b75b79b0df63f8cc9c3ec7e
esteban <estebantcs@gmail.com>
2018-02-18 21:31:24 -0500

    fixed data sql bug

--- src/main/resources/static/assets/js/app.js
+++ src/main/resources/static/assets/js/app.js
@@ -7,3 +7,43 @@ function checkTemporaryEmployee(contractType){
         $('#contract\\.endDate').removeAttr('required');
     }
 }
+
+// Tooltips Initialization
+$('#username').blur(function(event) {
+    event.target.checkValidity();
+}).bind('invalid', function(event) {
+
+    var $username = $('#username')
+
+    if( !$username.val() ) {
+
+        $username.attr("placeholder", "Este campo es obligatorio");
+
+        $username.focus();
+    }
+});
+
+$('#password').blur(function(event) {
+    event.target.checkValidity();
+}).bind('invalid', function(event) {
+
+    var $password = $('#password')
+
+    if( !$password.val() ) {
+
+        var $user = $('#username')
+
+        if(!$user.val()){
+
+        }
+        else{
+            $password.attr("placeholder", "Este campo es obligatorio");
+
+            $password.focus();
+        }
+
+
+
+    }
+});
+
