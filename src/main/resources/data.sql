0ce3752296de7aa4d421e6f070d17a19d27eed8f
Jose Quintero <jdqv9305@hotmail.com>
2018-02-22 01:43:01 -0500

    hard code role table ids in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- Insert role
-insert into role (name) values ('ROLE_USER');
-insert into role (name) values ('ROLE_ADMIN');
+insert into role (id,name) values (1,'ROLE_USER');
+insert into role (id,name) values (2,'ROLE_ADMIN');
 
---Insert one users (passwords are both 'password')
+-- Insert one users (passwords are both 'password')
 insert into usuarios (username,password,role_id,name) values ('admin','$2a$08$wgwoMKfYl5AUE9QtP4OjheNkkSDoqDmFGjjPE2XTPLDe9xso/hy7u',1,'admin');
 
 -- BOOTSTRAP/CATALOG SAMPLE DATA
